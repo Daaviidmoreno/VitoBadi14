9090c6c8ae1e1fa2143850bfb991dcc126cf6888
david <dmoreno029@ikasle.ehu.eus>
2025-11-21 13:46:03 +0100

    botones y subbotones metidos

--- public_html/js/principal.js
+++ public_html/js/principal.js
@@ -4,7 +4,7 @@ document.addEventListener("DOMContentLoaded", () => {
     const saludo = document.getElementById("saludoUsuario");
     const foto = document.getElementById("fotoUsuario");
 
-    // Si no hay usuario en sesión → fuera
+    // Si no hay usuario en sesión -> fuera
     if (!usuario) {
         window.location.href = "buscarHabitacionNoLogueado.html";
         return;
@@ -18,6 +18,35 @@ document.addEventListener("DOMContentLoaded", () => {
     document.getElementById("btnLogout").addEventListener("click", () => {
         sessionStorage.clear(); // borrar sesión por completo
         window.location.href = "buscarHabitacionNoLogueado.html";
+    });
+
+
+    // MENÚ CONSULTAR 
+    document.getElementById("btnGenerica").addEventListener("click", () => {
+        window.location.href = "buscarGenerica.html";
+    });
+
+    document.getElementById("btnGeo").addEventListener("click", () => {
+        window.location.href = "buscarGeolocalizacion.html";
+    });
+
+    //  MENÚ HABITACIONES 
+    document.getElementById("btnAnadirHab").addEventListener("click", () => {
+        window.location.href = "anadirHabitacion.html";
+    });
+
+    document.getElementById("btnMisHab").addEventListener("click", () => {
+        window.location.href = "misHabitaciones.html";
+    });
+
+    //  SOLICITUDES 
+    document.getElementById("btnVerSolicitudes").addEventListener("click", () => {
+        window.location.href = "verSolicitudes.html";
+    });
+
+    //  ALQUILERES
+    document.getElementById("btnVerAlquileres").addEventListener("click", () => {
+        window.location.href = "verAlquileres.html";
     });
 
 });
